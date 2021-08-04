f27b633d032f2bb22bbee61d87b23573074e279c
matyrobbrt <robertmateimoraru@gmail.com>
2021-08-04 16:36:06 +0300

    Issue Slot 36#1
    Redone how the Ultra Pearl keybinding works so it will not throw an error if the pearl is in slot 36.

--- src/main/java/com/matyrobbrt/reusableepearl/core/network/message/InputMessage.java
+++ src/main/java/com/matyrobbrt/reusableepearl/core/network/message/InputMessage.java
@@ -1,13 +1,14 @@
 package com.matyrobbrt.reusableepearl.core.network.message;
 
-import java.util.ArrayList;
 import java.util.function.Supplier;
 
+import com.matyrobbrt.reusableepearl.core.init.ItemInit;
+
 import net.minecraft.entity.item.EnderPearlEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.network.NetworkEvent;
@@ -15,16 +16,6 @@ import net.minecraftforge.fml.network.NetworkEvent;
 public class InputMessage {
 
 	public static int slot;
-
-	public static void findSlotMatchingUnusedItem(ItemStack p_194014_1_, PlayerEntity player) {
-		for (int i = 0; i < 36; ++i) {
-			ItemStack itemstack = player.inventory.getItem(i);
-			if (!player.inventory.getItem(i).isEmpty()
-					&& itemstack.getTagElement("id") == p_194014_1_.getTagElement("id")) {
-				slot = i;
-			}
-		}
-	}
 
 	public int key;
 
@@ -50,32 +41,24 @@ public class InputMessage {
 
 			ServerPlayerEntity playerEntity = context.getSender();
 			ServerWorld worldIn = playerEntity.getLevel();
-			//findSlotMatchingUnusedItem(new ItemStack(ItemInit.ULTRA_PEARL.get()), playerEntity);
-			String stringList = playerEntity.inventory.items.toString();
-			String stringList2 = "";
-			String subString = "";
-			
-			if (stringList.contains("ultra_pearl")) {
-				ArrayList<String> listArray = new ArrayList<String>(36);
-				
-				stringList = stringList.substring(1);
-				System.out.println(stringList);
-				
-				for (int i = 0; i <= 34; ++i) {
-					subString = stringList.substring(0, stringList.indexOf(","));
-					listArray.add(i, subString);
-					int counT = listArray.get(i).length() + 2;
-					stringList2 = stringList.substring(counT);
-					stringList = stringList2;
-					//System.out.println(listArray.get(i));
-					//System.out.println(stringList);
-					subString = "";
+			ResourceLocation upearl_id = ItemInit.ULTRA_PEARL.get().getRegistryName();
+			ItemStack stack = null;
+
+			// Off hand
+			if (playerEntity.getOffhandItem().getItem().getRegistryName() == upearl_id) {
+				stack = playerEntity.getOffhandItem();
+			} else {
+				// Inventory
+				for (int i = 0; i <= 35; ++i) {
+					ResourceLocation idLocation = playerEntity.inventory.getItem(i).getItem().getRegistryName();
+					if (idLocation == upearl_id) {
+						stack = playerEntity.inventory.getItem(i);
+						break;
+					}
 				}
-				listArray.add(35, stringList);
-				//System.out.println("index of ultra_pearl: " + listArray.indexOf("1 ultra_pearl"));
-				
-				ItemStack stack = playerEntity.inventory.getItem(listArray.indexOf("1 ultra_pearl"));
-				
+			}
+
+			if (stack != null) {
 				EnderPearlEntity entity = new EnderPearlEntity(worldIn, playerEntity);
 				entity.setOwner(playerEntity);
 				entity.shootFromRotation(playerEntity, playerEntity.xRot, playerEntity.yRot, 0.0F, 1.5F, 1.0F);
@@ -87,13 +70,12 @@ public class InputMessage {
 				if (DamageValue == 180) {
 					stack.shrink(1);
 				}
-		
-				
 			} else {
-				playerEntity.sendMessage(new StringTextComponent("\u00A74" + "You don't have an Ultra Ender Pearl on you!"),
+				playerEntity.sendMessage(
+						new StringTextComponent("\u00A74" + "You don't have an Ultra Ender Pearl on you!"),
 						playerEntity.getUUID());
 			}
-			//System.out.println(playerEntity.inventory.items);
+			// System.out.println(playerEntity.inventory.items);
 		});
 		context.setPacketHandled(true);
 	}
